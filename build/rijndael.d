ae9d39a449bfad6cbdd342d67899435b7a5fd2b0
Mega-Mario <thetotalworm@gmail.com>
2013-02-03 13:29:48 +0100

    Add gitignore and remove useless crap.

--- build/rijndael.d
+++
@@ -1,7 +0,0 @@
-rijndael.o: c:/Documents/wiihacking/SMGExtractor/source/rijndael.c \
- c:/devkitPro/libogc/include/gctypes.h \
- c:/Documents/wiihacking/SMGExtractor/source/rijndael.h
-
-c:/devkitPro/libogc/include/gctypes.h:
-
-c:/Documents/wiihacking/SMGExtractor/source/rijndael.h:
